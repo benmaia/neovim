2e2e2fbd6bf0479cbcab26ee1e3e7474af80e9aa
benmaia <godlinhomaia@gmail.com>
2022-05-28 01:16:02 +0100

    right dir

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ python3 -m pip install norminette
 cd ~/.local/share/fonts || curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 # Transfering my nvim config
-cp -r /home/$(whoami)/nvim /home/$(whoami)/.config
+cp -r /home/$(whoami)/nvim/nvim /home/$(whoami)/.config
 
 # Install Plugins
 nvim +PlugInstall +qall
