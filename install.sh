5972ec51a0ea8238114f324d276fd60811b01fb6
benmaia <godlinhomaia@gmail.com>
2022-05-28 01:14:52 +0100

    script

--- install.sh
+++ install.sh
@@ -11,48 +11,37 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update
-[ -f "/usr/bin/nvim" ] || sudo apt install neovim -y
+sudo apt install neovim -y
 
 # Install most recent version NodeJS && Yank
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-if [ -f "/usr/bin/npm" ]
-then
-	:
-else
-	sudo apt -y install nodejs
-	sudo apt -y install gcc g++ make
-	curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-	sudo apt update && sudo apt install yarn
-	yarn install
-fi
+sudo apt -y install nodejs
+sudo apt -y install gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+yarn install
 
 # Install pip
-[ -f "/usr/bin/pip" ] || sudo apt-get -y install python3-pip
+sudo apt-get -y install python3-pip
 
 # Install ripgrep
 sudo apt install ripgrep -y
 
 # Install Plugin Manager
-[ -f "/home/$(whoami)/.vim/autoload/plug.vim" ] || curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install Norminette
-if [ -f "/home/$(whoami)/.local/bin/norminette" ]
-then
-	:
-else
-	sudo apt-get install -y python3-launchpadlib
-	python3 -m pip install flake8
-	python3 -m pip install --upgrade pip setuptools
-	python3 -m pip install norminette
-fi
+sudo apt-get install -y python3-launchpadlib
+python3 -m pip install flake8
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install norminette
 
 # Install Nerd Font
-[ -f "~/.local/share/fonts/Droid Sans Mono for Powerline Nerd Font Complete.otf" ] || cd ~/.local/share/fonts || curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts || curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 # Transfering my nvim config
-[ -d "/home/$(whoami)/.config" ] || mkdir .config
-cp -r /home/$(whoami)/dotfiles_config/nvim /home/$(whoami)/.config
+cp -r /home/$(whoami)/nvim /home/$(whoami)/.config
 
 # Install Plugins
 nvim +PlugInstall +qall
